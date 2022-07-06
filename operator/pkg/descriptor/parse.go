2ee722c9484da19ba7d8729b63e3bc3190d25790
Aditya Bhatia <aditya.bhatia@sap.com>
2022-07-06 13:17:21 +0200

    handle digest based install & config for charts

---
+++ operator/pkg/descriptor/parse.go
@@ -0,0 +1,142 @@
+package descriptor
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"github.com/google/go-containerregistry/pkg/crane"
+	yaml2 "gopkg.in/yaml.v3"
+	"io"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"os"
+	"path/filepath"
+)
+
+type ManifestConfig struct {
+	name string
+}
+
+func ExtractTarGz(pathPattern string, repo string, module string, digest string) (string, error) {
+	reference := fmt.Sprintf("%s/%s@%s", repo, module, digest)
+	layer, err := crane.PullLayer(reference)
+	if err != nil {
+		return "", err
+	}
+
+	blobReadCloser, err := layer.Compressed()
+	if err != nil {
+		return "", fmt.Errorf("fetching blob resulted in an error %s: %w", layer, err)
+	}
+
+	uncompressedStream, err := gzip.NewReader(blobReadCloser)
+	if err != nil {
+		return "", fmt.Errorf("failure in NewReader() while extracting TarGz %s: %w", layer, err)
+	}
+
+	// check existing dir
+	installPath := filepath.Join(os.TempDir(), pathPattern)
+	dir, err := os.Open(installPath)
+	if err != nil && !os.IsNotExist(err) {
+		return "", fmt.Errorf("opening dir for installs caused an error %s: %w", layer, err)
+	}
+	if dir != nil {
+		return installPath, nil
+	}
+
+	// create new dir
+	if err = os.MkdirAll(installPath, 0770); err != nil {
+		return "", fmt.Errorf("failure in MkdirAll() while extracting TarGz %s: %w", layer, err)
+	}
+
+	// extract content to dir path
+	tarReader := tar.NewReader(uncompressedStream)
+	for true {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return "", fmt.Errorf("failed Next() while extracting TarGz %s: %w", layer, err)
+		}
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.Mkdir(filepath.Join(installPath, header.Name), 0755); err != nil {
+				return "", fmt.Errorf("failure in Mkdir() storage while extracting TarGz %s: %w", layer, err)
+			}
+		case tar.TypeReg:
+			outFile, err := os.Create(filepath.Join(installPath, header.Name))
+			if err != nil {
+				return "", fmt.Errorf("file create failed while extracting TarGz %s: %w", layer, err)
+			}
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				return "", fmt.Errorf("file copy storage failed while extracting TarGz %s: %w", layer, err)
+			}
+			outFile.Close()
+
+		default:
+			return "", fmt.Errorf("unknown type encountered while extracting TarGz %v in %s: %w",
+				header.Typeflag, filepath.Join(installPath, header.Name), err)
+		}
+
+	}
+
+	return installPath, nil
+}
+
+func DecodeYamlFromDigest(repo string, module string, digest string,
+	pattern string) (interface{}, error) {
+	reference := fmt.Sprintf("%s/%s@%s", repo, module, digest)
+	layer, err := crane.PullLayer(reference)
+	if err != nil {
+		return nil, err
+	}
+
+	// yaml is not compressed
+	blob, err := layer.Uncompressed()
+	if err != nil {
+		return nil, fmt.Errorf("fetching blob resulted in an error %s: %w", layer, err)
+	}
+
+	var configDecoder interface{}
+	err = yaml.NewYAMLOrJSONDecoder(blob, 2048).Decode(&configDecoder)
+
+	bytes, err := yaml2.Marshal(configDecoder)
+	if err != nil {
+		return nil, fmt.Errorf("yaml marshal for install config caused an error %s: %w", layer, err)
+	}
+
+	fullFilePath := filepath.Join(os.TempDir(), pattern, "installConfig.yaml")
+
+	// check existing file
+	file, err := os.Open(fullFilePath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, fmt.Errorf("opening file for install config caused an error %s: %w", layer, err)
+	}
+	if file != nil {
+		return configDecoder, nil
+	}
+
+	// create directory
+	if err := os.MkdirAll(filepath.Dir(fullFilePath), 0770); err != nil {
+		return nil, err
+	}
+
+	// create file
+	file, err = os.Create(fullFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("file creation for install config caused an error %s: %w", layer, err)
+	}
+
+	// write to file
+	if _, err = file.Write(bytes); err != nil {
+		return nil, fmt.Errorf("writing file for install config caused an error %s: %w", layer, err)
+	}
+
+	// close file
+	file.Close()
+
+	return configDecoder, err
+}
--- operator/pkg/manifest/operations.go
+++ operator/pkg/manifest/operations.go
@@ -33,6 +33,7 @@ type ChartInfo struct {
 	ChartName    string
 	ReleaseName  string
 	ClientConfig string
+	Overrides    string
 }
 
 type DeployInfo struct {
@@ -49,6 +50,9 @@ type RequestErrChan chan *RequestError
 
 type RequestError struct {
 	Ready             bool
+	ChartName         string
+	ClientConfig      string
+	Overrides         string
 	ResNamespacedName client.ObjectKey
 	Err               error
 }
@@ -92,8 +96,10 @@ func NewOperations(logger *logr.Logger, restConfig *rest.Config, releaseName str
 }
 
 func (o *Operations) getClusterResources(deployInfo DeployInfo, operation HelmOperation) (kube.ResourceList, kube.ResourceList, error) {
-	if err := o.repoHandler.Add(deployInfo.RepoName, deployInfo.Url); err != nil {
-		return nil, nil, err
+	if deployInfo.ChartPath == "" {
+		if err := o.repoHandler.Add(deployInfo.RepoName, deployInfo.Url); err != nil {
+			return nil, nil, err
+		}
 	}
 
 	manifest, err := o.getManifestForChartPath(deployInfo.ChartPath, deployInfo.ChartName, o.actionClient, o.args)
