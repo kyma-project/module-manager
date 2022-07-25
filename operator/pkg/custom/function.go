8d5c75ce2c448762e1e28fe50c1bb363e869da17
Aditya Bhatia <aditya.bhatia@sap.com>
2022-07-26 00:21:17 +0200

    chore: Remove default config as fallback, optional resource and others

--- operator/pkg/custom/function.go
+++ operator/pkg/custom/function.go
@@ -2,16 +2,14 @@ package custom
 
 import (
 	"context"
-	"k8s.io/client-go/rest"
-
 	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type CheckFnType func(ctx context.Context, manifestLabels map[string]string, namespacedName client.ObjectKey,
-	logger *logr.Logger, defaultRestConfig *rest.Config) (bool, error)
+	logger *logr.Logger) (bool, error)
 
 type Check interface {
 	CheckFn(ctx context.Context, manifestLabels map[string]string, namespacedName client.ObjectKey,
-		logger *logr.Logger, defaultRestConfig *rest.Config) (bool, error)
+		logger *logr.Logger) (bool, error)
 }
--- operator/pkg/custom/status.go
+++ operator/pkg/custom/status.go
@@ -2,8 +2,8 @@ package custom
 
 import (
 	"context"
-	"fmt"
 	"github.com/kyma-project/manifest-operator/operator/pkg/types"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -13,30 +13,72 @@ type Status struct {
 	client.Reader
 }
 
-func (c *Status) WaitForCustomResources(ctx context.Context, customWaitResource []types.CustomState) (bool, error) {
+//TODO: Define centrally
+const readyState = "Ready"
+
+func (c *Status) WaitForCustomResources(ctx context.Context, customWaitResource []types.CustomState,
+	stateResource *unstructured.Unstructured) (bool, error) {
+
+	// custom states
 	for _, res := range customWaitResource {
-		obj := unstructured.Unstructured{}
+		obj := &unstructured.Unstructured{}
 		obj.SetAPIVersion(res.APIVersion)
 		obj.SetKind(res.Kind)
 		namespacedName := client.ObjectKey{Name: res.Name, Namespace: res.Namespace}
-		if err := c.Get(ctx, namespacedName, &obj); client.IgnoreNotFound(err) != nil {
+
+		// if not found or any misc error occurred - return!
+		if err := c.Get(ctx, namespacedName, obj); err != nil {
 			return false, err
 		}
 
-		status, ok := obj.Object["status"]
-		if !ok {
-			return false, fmt.Errorf(".status object not found for %s", namespacedName.String())
+		state, err := getStateFieldFromUnstructured(obj)
+		if err != nil {
+			return false, err
 		}
 
-		state, ok := status.(map[string]interface{})["state"]
-		if !ok {
-			return false, fmt.Errorf(".status.state not found for %s", namespacedName.String())
+		if state != res.State {
+			return false, nil
+		}
+	}
+
+	// resource
+	if stateResource != nil {
+		existingStateResource := &unstructured.Unstructured{}
+		existingStateResource.SetGroupVersionKind(stateResource.GroupVersionKind())
+		customResourceKey := client.ObjectKey{
+			Name:      stateResource.GetName(),
+			Namespace: stateResource.GetNamespace(),
 		}
 
-		if state.(string) != res.State {
+		// if not found or any misc error occurred - return!
+		if err := c.Get(ctx, customResourceKey, existingStateResource); err != nil {
+			return false, err
+		}
+
+		state, err := getStateFieldFromUnstructured(existingStateResource)
+		if err != nil {
+			return false, err
+		}
+
+		if state != readyState {
 			return false, nil
 		}
 	}
 
 	return true, nil
+}
+
+func getStateFieldFromUnstructured(resource *unstructured.Unstructured) (string, error) {
+	path := field.NewPath("status")
+	status, ok := resource.Object["status"]
+	if !ok {
+		return "", field.NotFound(path, resource.Object)
+	}
+
+	path = path.Child("state")
+	state, ok := status.(map[string]interface{})["state"]
+	if !ok {
+		return "", field.NotFound(path, resource.Object)
+	}
+	return state.(string), nil
 }
