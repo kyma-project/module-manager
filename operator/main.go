149f497bc012d4ddb68a0857c1b23c7fc3580877
Jakob Möller <jakob.moeller@sap.com>
2022-08-26 10:14:47 +0200

    rename kyma-operator => lifecycle-manager (#74)

--- operator/main.go
+++ operator/main.go
@@ -21,18 +21,20 @@ import (
 	"os"
 	"time"
 
-	"github.com/kyma-project/manifest-operator/operator/controllers"
-	opLabels "github.com/kyma-project/manifest-operator/operator/pkg/labels"
-	"github.com/kyma-project/manifest-operator/operator/pkg/types"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/rest"
 
+	"github.com/kyma-project/manifest-operator/operator/controllers"
+	opLabels "github.com/kyma-project/manifest-operator/operator/pkg/labels"
+	"github.com/kyma-project/manifest-operator/operator/pkg/types"
+
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 
-	manifestv1alpha1 "github.com/kyma-project/manifest-operator/operator/api/v1alpha1"
 	apiExtensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+	manifestv1alpha1 "github.com/kyma-project/manifest-operator/operator/api/v1alpha1"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -101,7 +103,7 @@ func main() {
 		SelectorsByObject: cache.SelectorsByObject{
 			&v1.Secret{}: {
 				Label: labels.SelectorFromSet(
-					labels.Set{opLabels.ManagedBy: opLabels.KymaOperator},
+					labels.Set{opLabels.ManagedBy: opLabels.LifecycleManager},
 				),
 			},
 		},
