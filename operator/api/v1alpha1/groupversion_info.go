456e50dbee22f30a9731b83c72f29d5d52683386
Aditya Bhatia <aditya.bhatia@sap.com>
2022-08-29 10:33:53 +0200

    refactor: rename group component.kyma-project.io => operator.kyma-project.io (#76)
    refactor: rename API group component.kyma-project.io => operator.kyma-project.io

--- operator/api/v1alpha1/groupversion_info.go
+++ operator/api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the component v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=component.kyma-project.io
+// +groupName=operator.kyma-project.io
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "component.kyma-project.io", Version: "v1alpha1"} //nolint:gochecknoglobals
+	GroupVersion = schema.GroupVersion{Group: "operator.kyma-project.io", Version: "v1alpha1"} //nolint:gochecknoglobals
 
 	// GroupVersionResource is group version resource.
 	GroupVersionResource = GroupVersion.WithResource("manifests") //nolint:gochecknoglobals
--- operator/pkg/labels/component_labels.go
+++ operator/pkg/labels/component_labels.go
@@ -2,10 +2,9 @@ package labels
 
 const (
 	OperatorPrefix    = "operator.kyma-project.io"
-	ComponentPrefix   = "component.kyma-project.io"
 	Separator         = "/"
 	ComponentOwner    = OperatorPrefix + Separator + "kyma-name"
 	ManagedBy         = OperatorPrefix + Separator + "managed-by"
 	LifecycleManager  = "lifecycle-manager"
-	ManifestFinalizer = "component.kyma-project.io/manifest"
+	ManifestFinalizer = "operator.kyma-project.io/manifest"
 )
