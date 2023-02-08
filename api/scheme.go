9cfa90f40decc7c250dec248a16a34436f006780
D067928 <jakob.moeller@sap.com>
2023-02-08 13:31:42 +0100

    feat: v1beta1 manifest

--- api/scheme.go
+++ api/scheme.go
@@ -7,17 +7,17 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func AddToScheme(s *runtime.Scheme) error {
-	if err := v1alpha1.AddToScheme(s); err != nil {
+func AddToScheme(scheme *runtime.Scheme) error {
+	if err := v1alpha1.AddToScheme(scheme); err != nil {
 		return err
 	}
-	if err := v1beta1.AddToScheme(s); err != nil {
+	if err := v1beta1.AddToScheme(scheme); err != nil {
 		return err
 	}
-	if err := s.SetVersionPriority(v1beta1.GroupVersion, v1alpha1.GroupVersion); err != nil {
+	if err := scheme.SetVersionPriority(v1beta1.GroupVersion, v1alpha1.GroupVersion); err != nil {
 		return err
 	}
-	if err := s.AddConversionFunc(&v1alpha1.Manifest{}, &v1beta1.Manifest{},
+	if err := scheme.AddConversionFunc(&v1alpha1.Manifest{}, &v1beta1.Manifest{},
 		func(a, b interface{}, scope conversion.Scope) error {
 			return a.(*v1alpha1.Manifest).ConvertTo(b.(*v1beta1.Manifest))
 		}); err != nil {
